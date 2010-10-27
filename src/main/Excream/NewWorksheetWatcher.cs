d082619e06539f22b0f604bc6cffb133db429376
Tim Sample <sample2t@uregina.ca>
2010-10-27 00:44:08 -0600

    Changed the method of finding constrained cells in new worksheets
    We now use the Excel.Range.Find method for finding constrained cells,
        as it is substantially faster than iterating through the used range.

--- src/main/Excream/NewWorksheetWatcher.cs
+++ src/main/Excream/NewWorksheetWatcher.cs
@@ -12,9 +12,15 @@ namespace Excream {
             ws.Change += new Excel.DocEvents_ChangeEventHandler(ChangeWatcher.Change);
             DeactivateSheetWatcher deactivateWatcher = new DeactivateSheetWatcher(ws);
             ws.Deactivate += new Excel.DocEvents_DeactivateEventHandler(deactivateWatcher.Deactivate);
-            foreach(Excel.Range r in ws.UsedRange) {
-                if((string) r.Formula != "") {
-                    System.Windows.Forms.MessageBox.Show((string) r.Formula);
+            Excel.Range constrainedCell = ws.Cells.Find("CONSTRAIN", Type.Missing, Excel.XlFindLookIn.xlFormulas, Excel.XlLookAt.xlPart, Excel.XlSearchOrder.xlByRows, Excel.XlSearchDirection.xlNext, true, Type.Missing, Type.Missing);
+            Excel.Range firstFind = constrainedCell;
+            while(constrainedCell != null) {
+                constrainedCell.Calculate();
+                System.Windows.Forms.MessageBox.Show("Inside the loop!");
+                constrainedCell = ws.Cells.FindNext(constrainedCell);
+                if(constrainedCell.get_Address(Type.Missing, Type.Missing, Excel.XlReferenceStyle.xlA1, Type.Missing, Type.Missing) ==
+                   firstFind.get_Address(Type.Missing, Type.Missing, Excel.XlReferenceStyle.xlA1, Type.Missing, Type.Missing)) {
+                    break;
                 }
             }
         }
